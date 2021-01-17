f2c031745d38b28bc754a6bbbbcca2e30c308247
arjan <arjan_rekenmachine@hotmail.com>
2021-01-17 18:02:39 +0100

    Added total score calculation script.

---
+++ 2017Q/total_score.py
@@ -0,0 +1,10 @@
+import os
+total_score = 0
+scores = {}
+for file in os.listdir('output'):
+    assert file.endswith('.out')
+    *name,score = file[:-4].split('_')
+    name = '_'.join(name)
+    if name not in scores: scores[name] = 0
+    scores[name] = max(scores[name],int(score))
+print(sum(scores.values()))
