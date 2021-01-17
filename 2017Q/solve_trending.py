437de155f36051db472eb5daf21926fe8c7e4685
arjan <arjan_rekenmachine@hotmail.com>
2021-01-17 17:47:10 +0100

    Updated the solution for trending today. g

--- 2017Q/solve_trending.py
+++ 2017Q/solve_trending.py
@@ -1,6 +1,7 @@
 from check_sol_v2 import read_input, calc_score
 
-V,E,R,C,X,vidsize,endpoints,requests = read_input('trending_today.in')
+filename = 'trending_today.in'
+V,E,R,C,X,vidsize,endpoints,requests = read_input(filename)
 print("Capacity:", X)
 
 vidsizes = {}
@@ -19,11 +20,19 @@ while ctr < C and vidsizes:
         if len(vidsizes[v]) == 0: del vidsizes[v]
         w += v
         cache_set.add(vid)
-    caches.append(list(cache_set))
+    caches.append([len(caches)] + list(cache_set))
     print(w, len(cache_set))
     ctr += 1
 
 print("Items left:", ','.join(map(str,[v for vids in vidsizes.values() for v in vids])))
-print(ctr)
+print("Number of bins used:", ctr)
 
-print("Score:", calc_score(caches, vidsize, endpoints, requests, V, X))
+score = calc_score(caches, vidsize, endpoints, requests, V, X)
+print("Score:", score)
+
+import os
+file_name = os.path.join("output", '.'.join(filename.split('.')[:-1]) + '_' + str(score) + '.out')
+with open(file_name, 'w') as f:
+    f.write(str(len(caches))+"\n")
+    for videos in caches:
+        f.write(f"{' '.join(map(str,videos))}"+"\n")
