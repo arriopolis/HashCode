e0e62f422aa8c06b2923df0beb50bc1f392db1b3
Mathijs van Zon <32127224+IVIathijs@users.noreply.github.com>
2021-02-07 14:07:47 +0100

    Create read_input.py

---
+++ 2019F/read_input.py
@@ -0,0 +1,32 @@
+import sys
+
+class Instance:
+    def __init__(self, filepath):
+        f = open(filepath, 'r')
+        self.C, self.T, self.S = map(int, f.readline().split())
+        self.compiled_files = []
+        self.dependencies = []
+        self.target_files = []
+        for _ in range(self.C):
+            name, c, r = f.readline().split()
+            self.compiled_files.append((name, int(c), int(r)))
+            line = f.readline().split()
+            self.dependencies.append([])
+            if len(line):
+                for i in range(int(line[0])):
+                    self.dependencies[i].append(line[1:])
+        for _ in range(self.T):
+            name, d, g = f.readline().split()
+            self.target_files.append((name, d, g))
+
+
+def main():
+    instance = Instance(sys.argv[1])
+    print(instance.C, instance.T, instance.S)
+    for index in range(instance.C):
+        print(instance.compiled_files[index])
+        print(instance.dependencies[index])
+    print(instance.target_files)
+
+if __name__ == '__main__':
+    main()
