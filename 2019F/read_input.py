b0f9741773b7f295a848ff4546d6a0a77ba2736e
Mathijs van Zon <32127224+IVIathijs@users.noreply.github.com>
2021-02-07 14:12:58 +0100

    Update read_input.py

--- 2019F/read_input.py
+++ 2019F/read_input.py
@@ -5,15 +5,13 @@ class Instance:
         f = open(filepath, 'r')
         self.C, self.T, self.S = map(int, f.readline().split())
         self.compiled_files = []
-        self.dependencies = []
+        self.dependencies = [[] for i in range(self.C)]
         self.target_files = []
         for _ in range(self.C):
             name, c, r = f.readline().split()
             self.compiled_files.append((name, int(c), int(r)))
             line = f.readline().split()
-            self.dependencies = [[] for i in range(self.C)]
-            if len(line):
-                self.dependencies[_].append(line[1:])
+            self.dependencies[_] = line[1:]
         for _ in range(self.T):
             name, d, g = f.readline().split()
             self.target_files.append((name, d, g))
