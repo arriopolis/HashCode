7f485fb25f1a245264c8aad36279d6ca454bebb8
Mathijs van Zon <32127224+IVIathijs@users.noreply.github.com>
2021-02-07 15:22:01 +0100

    Update read_output.py

--- 2019F/read_output.py
+++ 2019F/read_output.py
@@ -13,20 +13,42 @@ class Solution:
             name, s = f.readline().split()
             self.compilation_steps.append((name, int(s)))
 
-    # def determine_score(self, instance):
-    #     self.ready_time_of_file_at_server = [[[] for file in range(instance.C)] for server in range(S)]
-    #     server_time = [0 for server in range(S)]
-    #     for step in self.compilation_steps:
-    #         name, server = step[0], step[1]
-    #         _, c, r = [file for file in instance.compiled_files][0]
-    #         server_time[server] += c
-    #         self.ready_time_of_file_at_server[server][] =
+    def determine_score(self, instance):
+        self.ready_time_of_file_at_server = [dict() for server in range(instance.S)]
+        server_time = [0 for server in range(instance.S)]
+        for step in self.compilation_steps:
+            name, server = step[0], step[1]
+            _, c, r = [file for file in instance.compiled_files if file[0] == name][0]
+            ready_time_of_latest_dependency = 0
+            if instance.dependencies_dict[name]:
+                ready_time_of_latest_dependency = max([self.ready_time_of_file_at_server[server][dependency] for dependency in instance.dependencies_dict[name]])
+            server_time[server] = max(ready_time_of_latest_dependency, server_time[server]) + c
+            self.ready_time_of_file_at_server[server][name] = server_time[server]
+            for other_server in range(instance.S):
+                if other_server != server:
+                    time_after_replication = server_time[server] + r
+                    if name not in self.ready_time_of_file_at_server[other_server].keys() or time_after_replication < self.ready_time_of_file_at_server[other_server][name]:
+                        self.ready_time_of_file_at_server[other_server][name] = time_after_replication
+        for server in range(instance.S):
+            print(self.ready_time_of_file_at_server[server])
+        score = 0
+        for name, target in instance.target_files_dict.items():
+            deadline, goal_points = target[0], target[1]
+            ready_times_of_target_at_servers = [self.ready_time_of_file_at_server[server][name] for server in range(instance.S) if name in self.ready_time_of_file_at_server[server].keys()]
+            if ready_times_of_target_at_servers:
+                time = min(ready_times_of_target_at_servers)
+                if time < deadline + 1:
+                    score += deadline - time + goal_points
+        return score
+
 
 def main():
-    solution = Solution(sys.argv[1])
+    solution = Solution(sys.argv[2])
     print(solution.E)
     for e in range(solution.E):
         print(solution.compilation_steps[e])
+    instance = Instance(sys.argv[1])
+    print(solution.determine_score(instance))
 
 
 if __name__ == '__main__':
