28c37e3d75b222415d0e1c71b11afee40153f91f
Mathijs van Zon <32127224+IVIathijs@users.noreply.github.com>
2021-02-07 14:40:22 +0100

    Added dicts to instance class

--- 2019F/read_input.py
+++ 2019F/read_input.py
@@ -6,16 +6,22 @@ class Instance:
         f = open(filepath, 'r')
         self.C, self.T, self.S = map(int, f.readline().split())
         self.compiled_files = []
+        self.compiled_files_dict = dict()
         self.dependencies = []
+        self.dependencies_dict = dict()
         self.target_files = []
+        self.target_files_dict = dict()
         for _ in range(self.C):
             name, c, r = f.readline().split()
             self.compiled_files.append((name, int(c), int(r)))
+            self.compiled_files_dict[name] = (int(c), int(r))
             line = f.readline().split()
             self.dependencies.append(tuple(line[1:]))
+            self.dependencies_dict[name] = tuple(line[1:])
         for _ in range(self.T):
             name, d, g = f.readline().split()
             self.target_files.append((name, d, g))
+            self.target_files_dict[name] = (d, g)
 
 
 def main():
@@ -24,7 +30,9 @@ def main():
     for index in range(instance.C):
         print(instance.compiled_files[index])
         print(instance.dependencies[index])
-    print(instance.target_files)
+    for index in range(instance.T):
+        print(instance.target_files[index])
+
 
 if __name__ == '__main__':
     main()
--- 2019F/read_output.py
+++ 2019F/read_output.py
@@ -1,8 +1,11 @@
 import sys
+from read_input import Instance
 
 
 class Solution:
     def __init__(self, filepath):
+        self.ready_time_of_file_at_server = []
+
         f = open(filepath, 'r')
         self.E = int(f.readline())
         self.compilation_steps = []
@@ -10,6 +13,14 @@ class Solution:
             name, s = f.readline().split()
             self.compilation_steps.append((name, int(s)))
 
+    # def determine_score(self, instance):
+    #     self.ready_time_of_file_at_server = [[[] for file in range(instance.C)] for server in range(S)]
+    #     server_time = [0 for server in range(S)]
+    #     for step in self.compilation_steps:
+    #         name, server = step[0], step[1]
+    #         _, c, r = [file for file in instance.compiled_files][0]
+    #         server_time[server] += c
+    #         self.ready_time_of_file_at_server[server][] =
 
 def main():
     solution = Solution(sys.argv[1])
