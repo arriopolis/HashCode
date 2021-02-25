a5bb7acc1ed0c10c998339d5fd97dbf704e264ac
erik <realtwister@gmail.com>
2021-02-25 20:38:52 +0100

    solution

--- 2021Q/read_input.py
+++ 2021Q/read_input.py
@@ -22,6 +22,8 @@ class Instance:
         return self.V * (self.F + self.D)
 
 
+from collections import deque
+
 class Solution:
     def __init__(self, solution, instance):
         assert isinstance(instance, Instance)
@@ -61,7 +63,15 @@ class Solution:
     @staticmethod
     def from_file(file, instance):
         with open(file) as f:
-            solution = None # parse solution from file
+            solution = list() # parse solution from file\
+
+            for _ in range(int(f.readline())):
+                iid = int(f.readline())
+                streets =[]
+                for _ in range(int(f.readline())):
+                    sn, time = f.readline().split(" ")
+                    streets.append((sn,int(time)))
+                solution.append((iid, streets))
         return Solution(solution, instance)
 
 
@@ -76,7 +86,55 @@ class Solution:
     def score(self):
         # calculate score
         score = 0
+        cars_tl = {s:deque() for s in self.instance.streets.keys()}
+        cars_in_transit = []
+        schedules = {iid: streets for iid, streets in self.solution}
+        current_tl = {iid: (0,streets[0][1]+1) for iid, streets in self.solution}
 
+
+        for i, path in enumerate(self.instance.paths):
+            cars_tl[path[0]].append(i)
+
+
+        for t in range(self.instance.D):
+            # print(f"---{t}----")
+            # print(cars_in_transit)
+            new_cars_in_transit =[]
+            active_streets = []
+            #calculate active streets
+            for iid, (idx, time_left) in current_tl.items():
+                active_streets.append(schedules[iid][idx][0])
+                time_left -=1
+                if time_left == 0:
+                    idx = (idx+1) % len(schedules[iid])
+                    time_left = schedules[iid][idx][1]
+                current_tl[iid] = (idx, time_left)
+            #     if iid == 0:
+            #         print(schedules[iid][idx][0], time_left, t)
+            # print(active_streets)
+
+
+            # cars pass active_streets
+            for sn in active_streets:
+                cars = cars_tl[sn]
+                if len(cars)>0:
+                    car = cars.popleft()
+                    idx = self.instance.paths[car].index(sn)+1
+                    next_street = self.instance.paths[car][idx]
+                    new_cars_in_transit.append((car, next_street, self.instance.streets[next_street][2]-1))
+
+            for car, street, time_left in cars_in_transit:
+                time_left -= 1
+                if time_left > 0:
+                    new_cars_in_transit.append((car,street,time_left))
+                else:
+                    if street == self.instance.paths[car][-1]:
+                        #calculate score
+                        # print(f"{car} is binnen")
+                        score += self.instance.F + self.instance.D - t
+                    else:
+                        cars_tl[street].append(car)
+            cars_in_transit = new_cars_in_transit
 
         return score
 
@@ -84,7 +142,7 @@ class Solution:
         import os
         score = self.score()
         print(f"New score is {score} which is {score/self.instance.score_upperbound()*100}% of upperbound.")
-        file_name = f"{self.instance.file_name.split('/')[-1][0]}_{score}.out"
+        file_name = f"{os.path.split(self.instance.file_name)[-1][0]}_{score}.out"
         file_path = os.path.join("output", file_name)
 
         with open(file_path, "w") as f:
@@ -95,3 +153,7 @@ class Solution:
                 for sn, time in streets:
                     f.write(f"{sn} {time}\n")
             # write this to file
+
+
+if __name__ == "__main__":
+    print(Solution.from_argv().score())
